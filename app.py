437f5a638a3c1d411ae5ed57bb11f3d76106e2e4
MatsuiMiyako <rowlet997@gmail.com>
2025-04-24 18:34:56 -0500

    Fixed templating format, and should be ready to implement AI chatbot to the site.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from website import create_app
 
 app = create_app()
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -18,3 +19,6 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
